8cb47b908a9e4cddf5caa6aef78917bd1a1dd79e
Victor Mazo <viamazoba@unal.edu.co>
2024-08-13 08:13:23 -0500

    feat: Se genera nueva página para editar los productos, además de su respectivo enrutamiento.

--- src/components/ProductsDetails.tsx
+++ src/components/ProductsDetails.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom'
 import { Product } from "../types"
 import { formatCurrency } from "../utils"
 
@@ -6,6 +7,8 @@ type ProductDetailsProps = {
 }
 
 export default function ProductsDetails({product} : ProductDetailsProps) {
+
+    const navigate = useNavigate()
 
     const isAvailable = product.availability
 
@@ -21,7 +24,13 @@ export default function ProductsDetails({product} : ProductDetailsProps) {
                 {isAvailable ? 'Disponible' : 'No Disponible' }
             </td>
             <td className="p-3 text-lg text-gray-800 ">
-                
+                <div className="flex gap-2 items-center">
+                    <button 
+                        onClick={ ()=> navigate(`/productos/${product.id}/editar`)}
+                        className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center">
+                        Editar
+                    </button>
+                </div>
             </td>
         </tr> 
     )
