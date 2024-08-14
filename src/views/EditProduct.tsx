e8dec008302dcd905110d3908fc8330a14d9e05a
Victor Mazo <viamazoba@unal.edu.co>
2024-08-14 07:45:12 -0500

    feat: Se avanza en la funcionalidad de editar productos, se añade su respectivo action y loader.

--- src/views/EditProduct.tsx
+++ src/views/EditProduct.tsx
@@ -1,8 +1,22 @@
-import { Link, Form, useActionData, ActionFunctionArgs, redirect } from "react-router-dom";
+import { Link, Form, useActionData, ActionFunctionArgs, redirect, LoaderFunctionArgs, useLoaderData } from "react-router-dom";
 import ErrorMessage from "../components/ErrorMessage";
-import { addProduct } from "../services/ProductService";
+import { getProductById, updateProduct } from "../services/ProductService";
+import { Product } from "../types";
 
-export async function action ({request} : ActionFunctionArgs ) {
+export async function loader({params} : LoaderFunctionArgs ) {
+    console.log(params)
+    if(params.id != undefined ) {
+        const product = await getProductById(+params.id)
+        if(!product) {
+            return redirect('/')
+        }
+
+        return product
+    }
+    return {}
+}
+
+export async function action ({request, params} : ActionFunctionArgs ) {
     const data = Object.fromEntries(await request.formData())
 
     let error = ''
@@ -16,14 +30,23 @@ export async function action ({request} : ActionFunctionArgs ) {
         return error
     }
 
-    await addProduct(data)
+    if(params.id !== undefined) {
+        await updateProduct(data, +params.id)
+    }
 
     return redirect('/')
 }
 
+const availabilityOptions = [
+    { name: 'Disponible', value: true},
+    { name: 'No Disponible', value: false}
+ ]
+
 export default function EditProduct() {
 
+    const product = useLoaderData() as Product
     const error = useActionData() as string
+    
     
     return(
         <>
@@ -57,8 +80,10 @@ export default function EditProduct() {
                         className="mt-2 block w-full p-3 bg-gray-50"
                         placeholder="Nombre del Producto"
                         name="name"
+                        defaultValue={product.name}
                     />
                 </div>
+
                 <div className="mb-4">
                     <label
                         className="text-gray-800"
@@ -70,7 +95,24 @@ export default function EditProduct() {
                         className="mt-2 block w-full p-3 bg-gray-50"
                         placeholder="Precio Producto. ej. 200, 300"
                         name="price"
+                        defaultValue={product.price}
                     />
+                </div>
+                <div className="mb-4">
+                    <label
+                        className="text-gray-800"
+                        htmlFor="availability"
+                    >Disponibilidad:</label>
+                    <select 
+                        id="availability"
+                        className="mt-2 block w-full p-3 bg-gray-50"
+                        name="availability"
+                        defaultValue={product?.availability?.toString()}
+                    >
+                        {availabilityOptions.map(option => (
+                        <option key={option.name} value={option.value.toString()}>{option.name}</option>
+                        ))}
+                    </select>
                 </div>
                 <input
                     type="submit"
