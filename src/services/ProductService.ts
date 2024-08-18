ed1dc7b2d2181db90c989a82206438fa4f25c849
Victor Mazo <viamazoba@unal.edu.co>
2024-08-17 21:15:51 -0500

    feat: Se finalizan operaciones de editar y registrar productos.

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,6 +1,7 @@
-import { safeParse } from 'valibot'
+import { safeParse, string, transform, number, pipe, parse }  from 'valibot'
 import axios from 'axios'
 import { DraftProductSchema, ProductsSchema, Product, ProductSchema } from '../types'
+import { toBoolean } from '../utils';
 
 type ProductData = {
     [k: string]: FormDataEntryValue;
@@ -64,6 +65,23 @@ export async function getProductById(id: Product['id']) {
 }
 
 export async function updateProduct( data: ProductData, id: Product['id'] ) {
-    console.log(data)
-    console.log(id)
+    try {
+        const NumberSchema = pipe(string(), transform(Number), number())
+
+        const result = safeParse(ProductSchema, {
+            id,
+            name: data.name,
+            price: parse(NumberSchema, data.price),
+            availability: toBoolean(data.availability.toString())
+        })
+        
+        if(result.success) {
+            const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`
+            await axios.put(url, result.output)
+        }
+
+    } catch (error) {
+        console.error(error)
+    }
+
 }
